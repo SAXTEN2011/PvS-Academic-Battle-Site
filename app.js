772c65459465b9622c89361102fa2cabe1af3834
SAXTEN2011 <amaxpierce@yahoo.com>
2015-04-22 07:37:50 -0500

    Routed /score

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var score = require('./routes/score.js');
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/score', score);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
